a890d823cdcecd894cf16e374047ee72fe142189
Anna P <amltrp@mail.ru>
2022-01-15 23:36:58 +0500

    add 5-2

--- src/presenter/film-presenter.js
+++ src/presenter/film-presenter.js
@@ -2,13 +2,10 @@ import CardView from '../view/card-view';
 import InfoView from '../view/info-view';
 import {remove, render, RenderPosition} from '../utils/render';
 import {replace} from '../utils/util';
-import CardListView from '../view/card-list-view';
 
 export default class FilmPresenter {
   #container = null;
   #siteFooterTemplate = null;
-
-  #filmsListComponent = new CardListView();
 
   #infoComponent = null;
   #cardComponent = null;
@@ -77,29 +74,26 @@ export default class FilmPresenter {
 
   #setCardComponentHandlers = () => {
     this.#cardComponent.setClickHandler(this.#openPopup);
-    this.#cardComponent.setAddToWatchClickHandler(this.#handleAddToWatchClick);
-    this.#cardComponent.setFavoriteClickHandler(this.#handleFavoriteClick);
-    this.#cardComponent.setWatchedClickHandler(this.#handleWatchedClick);
+    this.#cardComponent.setButtonsClickHandler(this.#handleAddToButtonsControlClick);
   }
 
   #setInfoPopupComponentHandlers = () => {
     this.#infoComponent.setClickHandler(this.#closePopup);
-    this.#infoComponent.setAddToWatchClickHandler(this.#handleAddToWatchClick);
-    this.#infoComponent.setFavoriteClickHandler(this.#handleFavoriteClick);
-    this.#infoComponent.setWatchedClickHandler(this.#handleWatchedClick);
+    this.#infoComponent.setButtonsClickHandler(this.#handleAddToButtonsControlClick);
   }
 
-  #handleAddToWatchClick = () => {
-    this.#film.isAddToWatchList = !this.#film.isAddToWatchList;
-    this.#changeData({...this.#film, isAddToWatchList: !this.#film.isAddToWatchList});
-  }
-
-  #handleWatchedClick = () => {
-    this.#changeData({...this.#film, isWatched: !this.#film.isWatched});
-  }
-
-  #handleFavoriteClick = () => {
-    this.#changeData({...this.#film, isFavorite: !this.#film.isFavorite});
+  #handleAddToButtonsControlClick = (value) => {
+    switch (value) {
+      case 'watchlist':
+        this.#changeData({...this.#film, isAddToWatchList: !this.#film.isAddToWatchList});
+        break;
+      case 'watched':
+        this.#changeData({...this.#film, isWatched: !this.#film.isWatched});
+        break;
+      case 'favorite':
+        this.#changeData({...this.#film, isFavorite: !this.#film.isFavorite});
+        break;
+    }
   }
 
   destroy = () => {
